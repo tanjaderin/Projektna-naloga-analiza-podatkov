c619fe35a05df8bb568f05faf3ab42dbbd5d472c
tanjaderin <tanja.derin88@gmail.com>
2019-10-29 13:19:38 +0100

    nekoncano

--- projektna.py
+++ projektna.py
@@ -1,6 +1,9 @@
 import csv
 import os
 import re
+import sys
+import requests
+import orodja
 
 mapa_strani = r'C:\Users\tanja\Desktop\projektna'
 stran = 'youtuberji_2.htm'
@@ -78,5 +81,47 @@ podatki = seznam_podatkov(mapa_strani, stran)
 zapisi_csv(podatki, polja, 'youtuberji_sez.csv')
 
 
+
+#with open('youtuberji.htm') as datoteka:
+#    vsebina = datoteka.read()
+
+
+
+vzorec_bloka = re.compile(
+    r'<div style=\"width: 860px; background: #fafafa; padding: 10px 20px; color:#444; font-size: 10pt; border-bottom: 1px solid #eee; line-height: \d0px;\">  '
+    r'(.*?)'
+    r'<div style=\"clear: both;\"></div></div>',
+    re.DOTALL)
+    
+
+vzorec_podatkov = re.compile(
+    r'<div style="float: left; width: 50px; color:#888;">(?P<mesto>.*?).*?'
+    r'<span style="font-weight: bold; color:#00bee7;">(?P<ocena>[AB]+[+-]*)</span>.*?'
+    r'<a href=".*">(?P<ime>.*?)</a>.*?'
+    r'<sup><i style="color:#aaa; padding-left: 5px;" title="Category: (?P<kategorija>.*?)" class=".*?" aria-hidden="false"></i></sup>.*?'
+    r'<div style="float: left; width: 80px;"><span style="color:#...;">(?P<objave>\d+,?\d+?)</span></div>.*?'
+    r'<div style="float: left; width: 150px;">\s*(?P<vpisani>\d+\.?\d*.).*?'
+    r'<span style="color:#555;">(?P<ogledi>.*?)</span>',
+    re.DOTALL)
+
+bloki = [] 
+stevec = 0 
+
+url = ('https://socialblade.com/youtube/top/5000/mostsubscribed')
+orodja.shrani_spletno_stran(url, 'zajeti_podatki')
+vsebina = orodja.vsebina_datoteke('zajeti_podatki')
+for ujemanje in re.findall(vzorec_bloka, vsebina):
+    bloki.append(ujemanje)
+    stevec += 1
+
+orodja.zapisi_json(bloki, 'obdelani-podatki/bloki.json')
+
+def page_to_ads(page_content):
+    """Funkcija poišče posamezne ogllase, ki se nahajajo v spletni strani in
+    vrne njih seznam"""
+    vzorec =r'<div style=\"width: 860px;.*?<div style=\"clear: both;\"></div></div>'
+    return re.findall(vzorec, page_content, re.DOTALL)
+
+page_to_ads('zajeti_podatki')
 
 
