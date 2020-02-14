d7d2192c66b9eda7b89e02b4730045cee2c8aedc
tanjaderin <tanja.derin88@gmail.com>
2020-02-14 17:25:50 +0100

    popravki

--- projektna.py
+++ projektna.py
@@ -11,23 +11,25 @@ url ='https://socialblade.com/youtube/top/5000/mostsubscribed'
 
 
 vzorec_bloka = re.compile(
-        r'<div style=\"width: 860px; background(.*?)<div style=\"clear: both;"',
+        r'<div style =\"width: 860px; background(.*?)<div style = \"clear: both;"',
         re.DOTALL)
 
+
 vzorec_podatkov = re.compile(
-    r'.*?<div style="float: left; width: 50px; color:#.*?">(?P<mesto>.*?)</div>.*?'
-    r'<span style="font-weight: bold; color:#.*?;">(?P<ocena>.*?)</span>.*?'
-    r'<a href=.*?>(?P<ime>.*?)</a>.*?'
-    r'<sup><i style="color:#aaa; padding-left: 5px;" title="Category: (?P<kategorija>.*?)" class=".*?" aria-hidden="false"></i></sup>.*?'
-    r'<div style="float: left; width: 80px;"><span style="color:#555;">(?P<objave>.*?)</span></div>.*?'
-    r'<div style="float: left; width: 150px;">(?P<vpisani>.*?)</div>.*?'
-    r'<div style="float: left; width: 150px;">.*?<span style="color:#555;">(?P<ogledi>.*?)</span>',
+    r'.*?<div style ="float: left; width: 50px; color:#.*?">(?P<mesto>.*?)</div>.*?'
+    r'<span style =\'font-weight: bold; color:#.*?;\'>(?P<ocena>.*?)</span>.*?'
+    r'<a href =.*?>(?P<ime>.*?)</a>.*?'
+    r'<sup><i style = "color:#aaa; padding-left: 5px;" title = "Category: (?P<kategorija>.*?)" class=".*?" aria-hidden="false"></i></sup>.*?'
+    r'<div style ="float: left; width: 80px;"><span style = "color:#555;">(?P<objave>.*?)</span></div>.*?'
+    r'<div style ="float: left; width: 150px;">(?P<vpisani>.*?)</div>.*?'
+    r'<div style ="float: left; width: 150px;">.*?<span style = "color:#555;">(?P<ogledi>.*?)</span>',
     flags = re.DOTALL)
  
 
 orodja.shrani_spletno_stran(url,'zajeti_podatki')
-vsebina = orodja.vsebina_datoteke('podatki')
+vsebina = orodja.vsebina_datoteke('zajeti_podatki')
 polja = ["mesto", "ocena", "ime", "kategorija", "objave", "vpisani","ogledi" ]
+
 
 bloki = [] 
 stevec = 0 
@@ -43,6 +45,7 @@ for ujemanje in re.finditer(vzorec_podatkov, vsebina):
     najdeni +=1
     seznam_slovarjev.append(ujemanje.groupdict())
 
+
 def pocisti_podatke(seznam):
     for slovar in seznam:
         for i in ["st", "nd", "th", "rd"]:
@@ -56,11 +59,10 @@ def pocisti_podatke(seznam):
         slovar["ogledi"] = int(slovar["ogledi"].replace(',', ''))
     return seznam
 
-
 print(najdeni)
 print(stevec)
 
-#orodja.zapisi_json(bloki, 'obdelani-podatki/bloki.json')
+orodja.zapisi_json(bloki, 'obdelani-podatki/bloki.json')
 orodja.zapisi_csv(pocisti_podatke(seznam_slovarjev), polja, 'obdelani-podatki/podatki.csv')
 
 
--- staro.py
+++ staro.py
@@ -1,42 +1,9 @@
-def vsebina_datoteke(mapa, ime_datoteke):
-    path = os.path.join(mapa, ime_datoteke)
-    with open(path, 'r', encoding='utf-8') as dat:
-        return dat.read()
-
-def pripravi_imenik(ime_datoteke):
-    imenik = os.path.dirname(ime_datoteke)
-    if imenik:
-        os.makedirs(imenik, exist_ok=True)
-
-def zapisi_csv(slovarji, imena_polj, ime_datoteke):
-    '''Iz seznama slovarjev ustvari CSV datoteko z glavo.'''
-    pripravi_imenik(ime_datoteke)
-    with open(ime_datoteke, 'w', encoding='utf-8') as csv_datoteka:
-        writer = csv.DictWriter(csv_datoteka, fieldnames=imena_polj)
-        writer.writeheader()
-        for slovar in slovarji:
-            writer.writerow(slovar)
-
-
 def bloki_iz_strani(ime_datoteke):
     vzorec_bloka = re.compile(
         r'<div style=\"width: 860px; background(.*?)<div style=\"clear: both;\".*?',
         re.DOTALL)
     kanali = re.findall(vzorec_bloka, ime_datoteke)
     return kanali
-
-def pocisti_podatke(podatki):
-    s = podatki.groupdict()
-    for i in ["st", "nd", "th", "rd"]:
-        if i in s["mesto"]:
-            s["mesto"] = s["mesto"].replace(i, "")
-    s["mesto"] = int(s["mesto"].replace(",", ""))
-    s["kanal"] = s["kanal"]
-    s["objave"] = int(s["objave"].replace(",", ""))
-    s["ocena"] = s["ocena"]
-    s["vpisani"] = int(s["vpisani"].replace(",", ""))
-    s["ogledi"] = int(s["ogledi"].replace(",", ""))
-    return s
 
 def podatki_iz_bloka(blok):
     vzorec_podatkov = re.compile(
